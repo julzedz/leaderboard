727cfadadbc012d4d6aa0c8d1955d404a552cc70
Edozie <edoziechukwuma@gmail.com>
2022-10-13 22:23:15 -0600

    create modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import getData from './modules/getdata.js';
 
 const form = document.querySelector('#form');
 form.addEventListener('submit', (e) => {
@@ -18,23 +19,10 @@ form.addEventListener('submit', (e) => {
   e.target.reset();
 });
 
-const display = (array) => {
-  const ul = document.querySelector('.scores-box');
-  ul.innerHTML = '';
-  array.forEach((item) => ul.innerHTML += `<li id="item">${item.user}: ${item.score}</li>`)
-}
-
-const gett = async () => {
-  const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6GL6p7oq2YDOWX6vDlM7/scores/", {method: "GET"});
-  const results = response.json();
-  results.then((object) => display(object.result));
-};
-
 document.querySelector('.refresh').addEventListener('click', () => {
-  gett();
+  getData();
 });
 
 window.addEventListener('load', () => {
-  gett();
+  getData();
 });
