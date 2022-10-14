853737c51bb9b5f5406ceea5b63ea2b18694daff
Jules Edozie <edoziechukwuma@gmail.com>
2022-10-14 07:03:15 +0100

    Update display.js

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,8 +1,8 @@
 const display = (array) => {
-  const ul = document.querySelector(".scores-box");
-  ul.innerHTML = "";
+  const ul = document.querySelector('.scores-box');
+  ul.innerHTML = '';
   array.forEach(
-    (item) => (ul.innerHTML += `<li id="item">${item.user}: ${item.score}</li>`)
+    (item) => (ul.innerHTML += `<li id="item">${item.user}: ${item.score}</li>`),
   );
 };
 
