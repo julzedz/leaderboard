46d01f3985a2eb622e5c247983f381b95bd735b7
Jules Edozie <edoziechukwuma@gmail.com>
2022-10-14 07:12:31 +0100

    Update getdata.js

--- src/modules/getdata.js
+++ src/modules/getdata.js
@@ -1,9 +1,9 @@
-import display from "./display.js";
+import display from './display.js';
 
 const getData = async () => {
   const response = await fetch(
-    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6GL6p7oq2YDOWX6vDlM7/scores/",
-    { method: "GET" }
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6GL6p7oq2YDOWX6vDlM7/scores/',
+    { method: 'GET',}
   );
   const results = response.json();
   results.then((object) => display(object.result));
