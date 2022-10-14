e3177b8f720f6449e328fef21a7b4fd2ee4b30ff
Jules Edozie <edoziechukwuma@gmail.com>
2022-10-14 06:59:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const user = document.querySelector('#user').value;
   const score = document.querySelector('#score').value;
-  fetch("https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6GL6p7oq2YDOWX6vDlM7/scores/", {
-    method: "POST",
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/6GL6p7oq2YDOWX6vDlM7/scores/', {
+    method: 'POST',
     body: JSON.stringify({
       user,
       score,
     }),
     headers: {
-      "Content-type": "application/json; charset=UTF-8",
+      'Content-type': 'application/json; charset=UTF-8',
     },
   });
   e.target.reset();
